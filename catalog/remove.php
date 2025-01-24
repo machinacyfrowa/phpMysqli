65f43ef1a1ba84e3197ea25b0968c872ef152e3b
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-01-24 14:16:40 +0100

    add html do remove confirmation page

--- catalog/remove.php
+++ catalog/remove.php
@@ -1,25 +1,43 @@
-<?php
-//$id to id książki do usunięcia
-$id = $_REQUEST['id'];
+<!DOCTYPE html>
+<html lang="pl">
 
-$db = new mysqli('localhost', 'root', '', 'book_catalog');
-$sql = "SELECT book.id, title.book_title AS title, 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="style.css">-->
+
+</head>
+
+<body>
+
+    <?php
+    //$id to id książki do usunięcia
+    $id = $_REQUEST['id'];
+
+    $db = new mysqli('localhost', 'root', '', 'book_catalog');
+    $sql = "SELECT book.id, title.book_title AS title, 
         CONCAT(author.first_name, ' ', author.last_name) AS author
         FROM book 
         LEFT JOIN author ON author.ID = book.author 
         LEFT JOIN title ON title.ID = book.title
         WHERE book.id = $id
         LIMIT 1";
-//pobranie danych o książce do usunięcia        
-$result = $db->query($sql);
-//wyciągnięcie pojedynczej książki
-$book = $result->fetch_assoc();
-?>
+    //pobranie danych o książce do usunięcia        
+    $result = $db->query($sql);
+    //wyciągnięcie pojedynczej książki
+    $book = $result->fetch_assoc();
+    ?>
 
-Czy jestes pewien, że chcesz usunąć poniższą książkę: <br>
-<?php
-echo $book['title'] . ' - ' . $book['author'];
-?>
-<br>
-<a href="delete.php?id=<?php echo $id; ?>">Tak</a>
-<a href="index.php">Nie</a>
+    Czy jestes pewien, że chcesz usunąć poniższą książkę: <br>
+    <?php
+    echo $book['title'] . ' - ' . $book['author'];
+    ?>
+    <br>
+    <a href="delete.php?id=<?php echo $id; ?>">Tak</a>
+    <a href="index.php">Nie</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
